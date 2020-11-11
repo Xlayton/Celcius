ceba58e7d96206dce8ef9ec7d5cb78bd94588a38
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-11-11 10:49:41 -0700

    News

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 console.log("Working")
+fetch("http://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=20ba8aef150a4b599827f02a43125c32")
+    .then(res => res.json())
+    .then(data => console.log(data))
